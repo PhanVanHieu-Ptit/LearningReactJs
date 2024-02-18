75df3d4e8d717e68ec9d7e0684735c9de13b846d
PhanVanHieu-Ptit <phanvanhieu090201@gmail.com>
2024-02-18 17:04:12 +0700

    accordion v2

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -24,26 +24,31 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       TODO
       {data.map((item, index) => (
         <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
           key={index}
           num={index + 1}
           title={item.title}
-          text={item.text}
-        />
+        >
+          {item.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen((current) => !current);
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -51,7 +56,7 @@ function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
