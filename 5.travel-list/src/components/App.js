9c8066d9600a3c62f837883972c71062a05e688b
PhanVanHieu-Ptit <phanvanhieu090201@gmail.com>
2024-02-18 16:02:35 +0700

    add checked, delete, sorted and divide component

---
+++ 5.travel-list/src/components/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((preState) => [...preState, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((preState) => preState.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((preState) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) {
+      setItems([]);
+    }
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
---
+++ 5.travel-list/src/components/Logo.js
@@ -0,0 +1,3 @@
+export default function Logo() {
+  return <h1>🍜 Far Away 💼 </h1>;
+}
